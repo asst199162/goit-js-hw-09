4d8066e8f3d8fb2940a392a533ab71bd8fd72af9
asst199162 <asst199162@gmail.com>
2022-06-22 21:27:11 +0300

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,26 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const refs = {
+    
+};
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const isDisabled = true;
+refs.startBtn.disbled = isDisabled;
+
+const timePicker = flatpickr(refs.dateInput, options);
+function dateCherck
